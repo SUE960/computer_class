2eac1c83c1fa341ecad0e9279c497441c4d32930
SUE <96976413+SUE960@users.noreply.github.com>
2025-12-13 21:49:24 +0900

    교육과정 섹션 제거
    - HomePage에서 Courses 컴포넌트 제거

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,6 @@ import Hero from '../components/Hero'
 import NoticeBoard from '../components/NoticeBoard'
 import About from '../components/About'
 import CampusSelector from '../components/CampusSelector'
-import Courses from '../components/Courses'
 import Footer from '../components/Footer'
 
 const HomePage = () => {
@@ -14,7 +13,6 @@ const HomePage = () => {
       <NoticeBoard />
       <About />
       <CampusSelector />
-      <Courses />
       <Footer />
     </div>
   )
